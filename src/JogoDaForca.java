e248e0b6345a0b509d2294a0e54e1c8bed675923
Gabriel Rosas <rosas.gabriel2007@gmail.com>
2026-04-01 21:31:59 -0300

    feat: Implementei atributos e métodos

--- src/JogoDaForca.java
+++ src/JogoDaForca.java
@@ -3,14 +3,21 @@ package src;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class JogoDaForca {
-	private ArrayList<String> bancoPalavras = new ArrayList<>();
+	private ArrayList<String> bancoPalavras, resultados = new ArrayList<>();
+	private ArrayList<Integer> ocorrencias = new ArrayList<>();
+	private Integer sorteioAtual, acertos, codidoPenalidade;
+	private String palavra, dica, nomePenalidade, resultado;
 	
-	public ArrayList<String> getBancoPalavras() {
-		return bancoPalavras;
+	
+	public String getDica() {
+		return dica;
 	}
+	
+
     public JogoDaForca(){
         try{
             String linha;
@@ -25,9 +32,28 @@ public class JogoDaForca {
         }
     }
     
+    public void iniciar() {
+    	Random aleatorio = new Random();
+    	sorteioAtual = aleatorio.nextInt(0, bancoPalavras.size());
+    	
+    	String[] palavraDica = bancoPalavras.get(sorteioAtual).split(",");
+    
+    	palavra = palavraDica[0];
+    	dica = palavraDica[1];
+    }
+    
+    public ArrayList<Integer> getOcorrencias(String letra){
+    	if (letra.contains(palavra)) {
+    		String[] caracteres = palavra.split("");
+    		for (int i=0; i<=caracteres.length; i++) {
+    			if (letra.equals(caracteres[i])) ocorrencias.add(i);
+    		}
+    	} 
+    	return ocorrencias;
+    }
     
     public static void main(String[] args) {
     	JogoDaForca x = new JogoDaForca();
-    	System.out.println(x.getBancoPalavras());
+    	
     }
 }
